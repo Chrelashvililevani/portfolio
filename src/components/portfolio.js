7f464b66d4914007b7869df46b6cae8aca577832
Chrelashvililevani <117888317+Chrelashvililevani@users.noreply.github.com>
2025-01-09 17:32:12 +0400

    Update portfolio.js

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Portfolio = () => {
   return (
-    <div className='main-contanier'>
+    <div className='main-container'>
       <header>
         <h1>Check projects done below</h1>
       </header>
@@ -17,6 +17,13 @@ const Portfolio = () => {
         <p>https://github.com/Chrelashvililevani/all-academy-hooks</p>
         <p>https://github.com/Chrelashvililevani/RegisterUser</p>
         <p>https://github.com/Chrelashvililevani/ol-academy-popup-with-handling-outside-click</p>
+        <p>https://github.com/Chrelashvililevani/ol-academy-popup-with-handling-outside-click</p>
+
+        <h2>Live Projects</h2>
+        <p><a href="https://art-project-ika.netlify.app/" target="_blank" rel="noopener noreferrer">Art Project</a></p>
+        <p><a href="https://perfume-ika.netlify.app/" target="_blank" rel="noopener noreferrer">Perfume Project</a></p>
+        <p><a href="https://boqo-wines.netlify.app/" target="_blank" rel="noopener noreferrer">Boqo Wines</a></p>
+        <p><a href="https://georgianvine.ge/" target="_blank" rel="noopener noreferrer">Georgian Vine</a></p>
       </main>
     </div>
   );
