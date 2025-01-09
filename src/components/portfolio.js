aadeb25fe503e43e7d8c0df26342b36793310aa3
Chrelashvililevani <117888317+Chrelashvililevani@users.noreply.github.com>
2025-01-09 17:35:47 +0400

    Update portfolio.js

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -7,18 +7,6 @@ const Portfolio = () => {
         <h1>Check projects done below</h1>
       </header>
       <main className='portfolio-main'>
-        <h2>Git Links</h2>
-        <p>https://github.com/Chrelashvililevani/ol-academy-advanced-js-homeworks</p>
-        <p>https://github.com/Chrelashvililevani/ol-academy-react-todo</p>
-        <p>https://github.com/Chrelashvililevani/Art-Project</p>
-        <p>https://github.com/Chrelashvililevani/ol-academy-react-todo-useEffect</p>
-        <p>https://github.com/Chrelashvililevani/ol-academy-context-menu</p>
-        <p>https://github.com/Chrelashvililevani/ol-academy-popup-with-handling-outside-click</p>
-        <p>https://github.com/Chrelashvililevani/all-academy-hooks</p>
-        <p>https://github.com/Chrelashvililevani/RegisterUser</p>
-        <p>https://github.com/Chrelashvililevani/ol-academy-popup-with-handling-outside-click</p>
-        <p>https://github.com/Chrelashvililevani/ol-academy-popup-with-handling-outside-click</p>
-
         <h2>Live Projects</h2>
         <p><a href="https://art-project-ika.netlify.app/" target="_blank" rel="noopener noreferrer">Art Project</a></p>
         <p><a href="https://perfume-ika.netlify.app/" target="_blank" rel="noopener noreferrer">Perfume Project</a></p>
